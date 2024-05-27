ec2b063ff322bba9df2b3eb69acd6eb98d90f5d2
belu-10 <148562666+belu-10@users.noreply.github.com>
2024-05-27 14:03:22 -0300

    Commit 2 - Contraseña funciona
    Presionas acep y compara la contraseña

---
+++ Assets/LoginManagerr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LoginManagerr : MonoBehaviour
+{
+    public InputField inputNombre;
+    private string contra = "hola"; 
+
+    public void BotonAceptar()
+    {
+        if (inputNombre.text == contra)
+        {
+            Debug.Log("Access granted");
+        } 
+
+        else
+        {
+            Debug.Log("Access denied"); 
+        }
+    }
+}
